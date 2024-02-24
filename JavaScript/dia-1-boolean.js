4f040dc1630f027c1f9a0b159444bcd2b786413c
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2024-02-24 14:58:04 -0300

    Create dia-1-boolean.js

---
+++ JavaScript/dia-1-boolean.js
@@ -0,0 +1,37 @@
+    console.log('Ola pessoas!')
+    console.log('1º Dia de #7DaysOfCode!');
+
+    // Explicação do desafio 
+
+    console.log( false == '0' );
+    console.log( null == undefined );
+    console.log( " \t\r\n" == 0 );
+    console.log( ' ' == 0 );
+
+    // Começa o Desafio
+
+    let numeroUm = 1;
+    let stringUm = '1';
+    let numeroTrinta = 30;
+    let stringTrinta = '30';
+    let numeroDez = 10;
+    let stringDez = '10';
+
+    if (numeroUm == stringUm) {
+        console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+    } else {
+        console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+    }
+
+    if (numeroTrinta == stringTrinta) {
+        console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+    } else {
+        console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor')
+    }
+
+    if (numeroDez == stringDez) {
+        console.log('As variáveis numeroDez e strinDez tem o mesmo valor, mas tipos diferentes')
+        
+    } else {
+        console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+    }
