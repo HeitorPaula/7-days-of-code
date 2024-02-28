a3473eac54d984613f9894b91749f371ab5d8147
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2024-02-27 22:15:16 -0300

    Update dia-3-condicionais.js

--- JavaScript/dia-3/dia-3-condicionais.js
+++ JavaScript/dia-3/dia-3-condicionais.js
@@ -5,20 +5,30 @@ console.log('3º Dia de #7DaysOfCode!');
 
 const area = prompt('Por qual área você quer seguir, Front-End ou Back-End?');
 
+let linguagem = "";
+
 if (area === "Front-End") {
-    prompt('Você quer seguir na área de Front-End! Mas você quer aprender React ou Vue?')
+    prompt('Você quer seguir na área de Front-End! Mas você quer aprender React ou Vue?');
+} else if (area === "Back-End") {
+    prompt('Você quer seguir na área de Back-End! Mas você quer aprender C# ou Java?');
+} else {
+    alert('Você não inseriu uma área válida!');
 };
 
-if (area === "Back-End") {
-    prompt('Você quer seguir na área de Back-End! Mas você quer aprender C# ou Java?')
+const especialidadeOuFullstack = prompt('Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack');
+
+if (especialidadeOuFullstack == 1) {
+    alert(`Continue se especializando em ${linguagem} para dominar a área de ${area}!`);
+} else if (especialidadeOuFullstack == 2) {
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
+} else {
+    alert('Você não inseriu um valor válido!');
 };
 
-prompt(`Você quer seguir se especializando em ${area}, ou continuar se desenvolvendi para se tornar Fullstack?`);
+let msg = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite "ok" em caso positivo.');
 
-const tecnologias = prompt(`Tem mais alguma tecnologia que você gostaria de aprender?`)
-
-while (tecnologias) {
-    prompt(`Tem mais alguma tecnologia que você gostaria de aprender?`)
-};
-
-alert('Obrigado pela sua atenção!');
+while (msg === "ok") {
+    let novaTecnologia = prompt('Qual?');
+    alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`);
+    msg = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite "ok" em caso positivo.');
+}
